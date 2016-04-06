46749d286d76a8a36c1cd0ed0c9252b23b3b9308
Taylor Ashton <taylor.ashton17@gmail.com>
2016-04-06 11:51:51 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
 
 var server = app.listen(process.env.PORT || 3000, function() {
   var port = server.address().port;
   console.log('listening on port ', port);
+  console.log("hello world");
 })
 
 function handleGet(request, response) {
   console.log('received a request!');
-  res.sendFile(path.join(__dirname, './index.html'));
+  response.sendFile(path.join(__dirname, './index.html'));
 };
 app.get('/', handleGet);
